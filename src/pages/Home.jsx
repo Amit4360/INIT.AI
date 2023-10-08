85220c9c0ad02b714e6429cfd25f00e65aeb6ba9
Ayush <ayush.up105@gmail.com>
2023-10-08 22:16:58 +0530

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import AboutUs from '../components/AboutUs'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <AboutUs/>
+      <Footer/>
     </>
   )
 }
